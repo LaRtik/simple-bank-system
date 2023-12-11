320211e1571889b82d56cdbee532f67d0a29ed57
Ilya Lazuk <i.lazuk45@gmail.com>
2023-12-11 17:59:27 +0300

    fixed login process

--- bank_system/accounts/forms.py
+++ bank_system/accounts/forms.py
@@ -7,14 +7,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(max_length=65)
+    username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 
 
