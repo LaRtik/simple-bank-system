e2b3bbb0c03a13374a3b3b99a76cae712502dab1
Ilya Lazuk <i.lazuk45@gmail.com>
2023-12-11 18:12:51 +0300

    added deposit processing

--- bank_system/credit/middleware.py
+++ bank_system/credit/middleware.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 import decimal
+
+from bank_account.models import BankAccount
 from credit.models import CreditTransaction
 
 class CreditPaymentMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.last_time_check = datetime.now()
+        self.last_time_check = None
         # One-time configuration and initialization.
 
     def __call__(self, request):
@@ -13,8 +15,10 @@ class CreditPaymentMiddleware:
         # the view (and later middleware) are called.
         response = self.get_response(request)
         
-        if (datetime.now() - self.last_time_check).total_seconds() > 24 * 60 * 60:
+        if not self.last_time_check or (datetime.now() - self.last_time_check).total_seconds() > 24 * 60 * 60:
             self.last_time_check = datetime.now()
+
+            # checking credits
             transactions = CreditTransaction.objects.filter(dt__lt=datetime.now())
             for transaction in transactions:
                 if transaction.amount > 0:
@@ -28,6 +32,14 @@ class CreditPaymentMiddleware:
                     else:
                         transaction.amount *= decimal.Decimal(1.011)
                         transaction.save()
+
+            # checking deposits
+            bank_accounts = BankAccount.objects.all()
+            for bank_account in bank_accounts:
+                bank_account.balance *= decimal.Decimal(1.0002)
+                bank_account.save()
+            
+
         # Code to be executed for each request/response after
         # the view is called.
 
