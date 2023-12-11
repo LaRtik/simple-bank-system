4db7f879b88d1ef231861f79a2e711594c521499
Ilya Lazuk <i.lazuk45@gmail.com>
2023-12-11 07:55:41 +0300

    fixed telegram widget url

--- bank_system/bank_system/settings.py
+++ bank_system/bank_system/settings.py
@@ -131,5 +131,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-PROXY_DOMAIN_URL = "http://tricky-geckos-guess.loca.lt"
